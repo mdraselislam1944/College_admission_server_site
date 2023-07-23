eae7f1a22add7227d4d1ef91b53069116dab0153
mdraselislam1944 <mdraselislam1944@gmail.com>
2023-07-24 01:56:20 +0600

    'add-all-part'

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express=require('express');
+const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
@@ -22,50 +22,88 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
-    const AllProductsCollection=client.db('MRGOVTCOLLEGEDB').collection('mrgovtcollege');
-    app.post("/addCollege",async(req,res)=>{
-      const data=req.body;
-      const result=await AllProductsCollection.insertOne(data);
+    const AllProductsCollection = client.db('MRGOVTCOLLEGEDB').collection('mrgovtcollege');
+    app.post("/addCollege", async (req, res) => {
+      const data = req.body;
+      const result = await AllProductsCollection.insertOne(data);
       res.send(result);
     })
-    app.get('/showCollege',async(req,res)=>{
-      const result=await AllProductsCollection.find().limit(3).toArray();
+    app.get('/showCollege', async (req, res) => {
+      const result = await AllProductsCollection.find().limit(3).toArray();
       res.send(result);
     })
-    app.get('/college',async(req,res)=>{
-      const result=await AllProductsCollection.find().toArray();
+    app.get('/college', async (req, res) => {
+      const result = await AllProductsCollection.find().toArray();
       res.send(result);
     })
-    app.get('/individual/:id',async(req,res)=>{
-      const id=req.params.id;
-      const result=await AllProductsCollection.findOne({_id:new ObjectId(id)});
+    app.get('/individual/:id', async (req, res) => {
+      const id = req.params.id;
+      const result = await AllProductsCollection.findOne({ _id: new ObjectId(id) });
       res.send(result);
     })
 
     app.get("/colleges/:individualCollege", async (req, res) => {
       const collegeName = req.params.individualCollege;
-      try{
+      try {
         const result = await AllProductsCollection.find({ collegeName: { $regex: new RegExp(collegeName, 'i') } }).toArray();
         res.send(result);
-      } catch{
+      } catch {
         console.error('Error retrieving data:', error);
         res.status(500).send('Error retrieving data');
       }
     });
 
 
-    const studentInformation=client.db("MRGOVTCOLLEGEDB").collection("studentInformation");
-    app.post("/studentsAdd",async(req,res)=>{
-      const data=req.body;
-      const result=await studentInformation.insertOne(data);
+    const studentInformation = client.db("MRGOVTCOLLEGEDB").collection("studentInformation");
+    app.post("/studentsAdd", async (req, res) => {
+      const data = req.body;
+      const result = await studentInformation.insertOne(data);
       res.send(result);
     })
-    app.get("/studentsClass/:email",async(req,res)=>{
-      const email=req.params.email;
-      const query={email:email};
-      const result=await studentInformation.find(query).toArray();
+    app.get("/studentsClass/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await studentInformation.find(query).toArray();
+      res.send(result);
+    })
+
+    app.get("/editInformation/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await studentInformation.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    })
+    app.patch('/updateStudent/:id', async (req, res) => {
+      const id = req.params.id;
+      const student = req.body;
+      const filter = { _id: new ObjectId(id) }
+      const option = { upsert: true }
+      const updateProduct = {
+        $set: {
+          candidateName: student.candidateName,
+          Subject: student.Subject,
+          email: student.email,
+          phone: student.phone,
+          address:student.address,
+          date:student.date,
+          image:student.image,
+        }
+      }
+      const result = await studentInformation.updateOne(filter, updateProduct, option);
+      res.send(result);
+    })
+
+    const Feedback = client.db("MRGOVTCOLLEGEDB").collection("feedback");
+
+    app.post("/information",async(req,res)=>{
+      const body=req.body;
+      const result=await Feedback.insertOne(body);
+      res.send(result);
+    })
+
+    app.get("/getInformation",async(req,res)=>{
+      const result=await Feedback.find().toArray();
       res.send(result);
     })
 
@@ -82,10 +120,9 @@ run().catch(console.dir);
 
 app.get('/', (req, res) => {
 
-    res.send('server is running');
-  })
-  app.listen(port, () => {
-    console.log(`The server port number is ${port}`);
-  })
-  
-  
+  res.send('server is running');
+})
+app.listen(port, () => {
+  console.log(`The server port number is ${port}`);
+})
+
